05c94821ffa16d160070bb302b92a7b97504f177
Túlio Cesar Martins Pereira <tulio.martins@rockcontent.com>
2020-07-09 10:20:16 -0300

    Generate token when user is created

--- backend/app/app/crud/crud_user.py
+++ backend/app/app/crud/crud_user.py
@@ -6,18 +6,24 @@ from app.core.security import get_password_hash, verify_password
 from app.crud.base import CRUDBase
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate
-
+from app.core import security
+from app.core.config import settings
+from datetime import timedelta
 
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
     def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
         return db.query(User).filter(User.email == email).first()
 
     def create(self, db: Session, *, obj_in: UserCreate) -> User:
+        access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
         db_obj = User(
             email=obj_in.email,
             hashed_password=get_password_hash(obj_in.password),
             full_name=obj_in.full_name,
             is_superuser=obj_in.is_superuser,
+            token=security.create_access_token(
+                obj_in.email, expires_delta=access_token_expires
+            )
         )
         db.add(db_obj)
         db.commit()
