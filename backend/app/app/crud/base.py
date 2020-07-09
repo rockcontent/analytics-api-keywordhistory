b0513dc2529b73cce1157e1425d53db66c44b395
Túlio Cesar Martins Pereira <tulio.martins@rockcontent.com>
2020-07-09 10:47:04 -0300

    Change method of get user

--- backend/app/app/crud/base.py
+++ backend/app/app/crud/base.py
@@ -24,7 +24,11 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         self.model = model
 
     def get(self, db: Session, id: Any) -> Optional[ModelType]:
-        return db.query(self.model).filter(self.model.id == id).first()
+        try:
+            id = int(id)
+            return db.query(self.model).filter(self.model.id == id).first()
+        except Exception as e:
+            return db.query(self.model).filter(self.model.email == id).first()
 
     def get_multi(
         self, db: Session, *, skip: int = 0, limit: int = 100
