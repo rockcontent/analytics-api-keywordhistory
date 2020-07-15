022139bea5a343712b99114b75dcc5b33113f92e
Túlio Cesar Martins Pereira <tulio.martins@rockcontent.com>
2020-07-15 09:26:05 -0300

    Add endpoint DomainOrganic

--- backend/app/app/api/api_v1/endpoints/domains.py
+++ backend/app/app/api/api_v1/endpoints/domains.py
@@ -44,5 +44,48 @@ async def domain_domains(
             }
             response.append(dm)
 
-        crud.phrase_all.create(db=db, obj_in=response)
+        crud.domain_domains.create(db=db, obj_in=response)
+        return response
+
+
+@router.get("/domain_organic/{domain}/{database}",
+            response_model=List[schemas.domain_domains.DomainDomains],
+            description="This report allows users to compare up to five domains by common keywords, "
+                        "unique keywords, all keywords, or search terms that are unique to the "
+                        "first domain.")
+async def domain_organic(
+        domain: str,
+        database: str,
+        current_user: models.User = Depends(deps.get_current_active_user),
+        db: Session = Depends(deps.get_db)
+) -> Any:
+
+    domain_organic_data = crud.domain_organic.get_by_domain(db, domain=domain, database=database)
+
+    if len(domain_organic_data) > 0:
+        return domain_organic_data
+    else:
+        response = []
+        domains = await client.domain_organic(domain, database=database)
+
+        for d in domains:
+            dm = {
+                'domain': domain,
+                'database': database,
+                'keyword': d.get('Keyword').encode('utf8'),
+                'position': int(d.get('Position')),
+                'previous_position': int(d.get('Previous Position')),
+                'position_difference': int(d.get('Position Difference')),
+                'search_volume': int(d.get('Search Volume')),
+                'cpc': d.get('CPC'),
+                'competition': float(d.get('Competition')),
+                'url': d.get('Url'),
+                'traffic': float(d.get('Traffic (%)')),
+                'traffic_cost': float(d.get('Traffic Cost (%)')),
+                'number_results': int(d.get('Number of Results')),
+                'trends': d.get('Trends')
+            }
+            response.append(dm)
+
+        crud.domain_organic.create(db=db, obj_in=response)
         return response
--- backend/app/app/models/domain_organic.py
+++ backend/app/app/models/domain_organic.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date, DECIMAL
+from sqlalchemy import Column, Integer, String, DECIMAL, BigInteger
 
 from app.db.base_class import Base
 
@@ -17,5 +17,5 @@ class DomainOrganic(Base):
     url = Column(String, nullable=False)
     traffic = Column(DECIMAL, nullable=False, default=0.0)
     traffic_cost = Column(DECIMAL, nullable=False, default=0.0)
-    number_results = Column(Integer, nullable=False, default=0)
+    number_results = Column(BigInteger, nullable=False, default=0)
     trends = Column(String, index=True, nullable=False)
