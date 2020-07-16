932d8a3f13c8e16ac405ee3625b69c5914977bd6
Túlio Cesar Martins Pereira <tulio.martins@rockcontent.com>
2020-07-16 16:58:22 -0300

    Add support to logging all requests in console

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,3 +1,9 @@
+from os import path
+import random
+import string
+import time
+from urllib.request import Request
+import logging
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
@@ -7,6 +13,26 @@ from app.core.config import settings
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
+
+log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.conf')
+logging.config.fileConfig(log_file_path, disable_existing_loggers=False)
+
+logger = logging.getLogger(__name__)
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    idem = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    logger.info(f"rid={idem} start request path={request.url.path}")
+    token = request.headers.get("authorization")
+    logger.info(f"rid={idem} application token: {token}")
+    start_time = time.time()
+    response = await call_next(request)
+
+    process_time = (time.time() - start_time) * 1000
+    formatted_process_time = '{0:.2f}'.format(process_time)
+    logger.info(f"rid={idem} completed_in={formatted_process_time}ms status_code={response.status_code}")
+
+    return response
 
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
